17f8098e168111d848aabd5fa85304bc98bab194
dos21h <dos21h@2c3dfb34-371d-ebf1-ef7c-1d4fb77a72ab>
2011-01-06 17:41:04 +0000

    Rearrange files. File names. Chnage structure of programm

---
+++ trunk/card.lisp
@@ -0,0 +1,24 @@
+
+(defun randomize-cards ()
+)
+
+(defun get-card-number ()
+	()
+)
+
+(defun output-cards ()
+	()
+)
+
+;check if is same cards
+(defun check-cards ()
+	()
+)
+
+(defun throw-cards ()
+	()
+)
+
+(defun change-cards ()
+	()
+)
---
+++ trunk/player.lisp
@@ -0,0 +1,20 @@
+;data structure for every player in da game
+(defstruct gplayer 
+	name	;string
+	cards	;list
+	endgame ;boolean true if outside game
+)
+
+(defun make-null-gplayer (name)
+	()
+)
+
+
+(defun get-player-number ()
+	() 
+)
+
+
+(defun if-player-loser ()
+	()
+)
