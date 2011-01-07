a4423efce460e816442d927aa2e3576a187b2256
dos21h <dos21h@2c3dfb34-371d-ebf1-ef7c-1d4fb77a72ab>
2011-01-07 14:34:32 +0000

    User Input functions. Player number, player name

--- game.lisp
+++ game.lisp
@@ -4,15 +4,25 @@
 	(random max-n)
 )
 
+;player number is between 2 and 10
 (defun input-player-number ()
 	(prog ()
-		(loop while (not nil) do ()
+		(print "Input player number:")
+		(setq player-number (read))
+		(loop while (not (and (numberp player-number) (> player-number 1) (< player-number 11) (not (floatp player-number)))) do
+			(print "Input number [2..10]:")
+			(setf player-number (read))
 		)
+		(return player-number)
 	)
 )
 
 (defun input-player-name ()
-	()
+	(prog ()
+		(print "Input player name: ")
+		(setq player-name (read))
+		(return player-name)
+	)
 )
 
 (defun input-card-name ()
--- main.lisp
+++ main.lisp
@@ -5,10 +5,6 @@
 
 (init-screen)
 
-(setq p1 (make-null-gplayer "0"))
-(print p1)
-
-(generate-player-ring 6)
-(print-players player-ring)
+(print (cards-nominal 12))
 
 (end-screen)
